3f5e640a57ae56c0cd5dd7c8f9d25279e04568b9
anowarulkarim <anowarulkarim8@gmail.com>
2024-08-31 15:40:40 +0600

    jobs deleted

--- jobs/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . models import job
-from . models import Recuter
-# Register your models here.
-admin.site.register(job)
-admin.site.register(Recuter)
--- jobs/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-from clientUser import views as client_views
-app_name="jobs"
-urlpatterns=[
-    path("",views.alljobs,name="alljobs"),
-]    
