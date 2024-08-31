b2187ad5679d9061d36ac193292c03bf43c4c8d3
anowarulkarim <anowarulkarim8@gmail.com>
2024-09-01 03:29:31 +0600

    job Circular and recruter updated

--- JobCircular/urls.py
+++ JobCircular/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 app_name="jobs"
 urlpatterns=[
-    path("",views.alljobs,name="allJobs")
+    path("",views.alljobs,name="allJobs"),
+    path("postjob/",views.postjob,name="postjob")
 ]
--- JobCircular/views.py
+++ JobCircular/views.py
@@ -1,13 +1,22 @@
+from django import forms
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
-
+from . models import Circular
 # Create your views here.
+class job(forms.Form):
+    titel=forms.CharField(max_length=50)
+    salary=forms.IntegerField()
+    last_date=forms.DateField()
 def alljobs(request):
         if request.session.get("login"):
+            Circulars=Circular.objects.all()
             return render(request,"JobCircular/jobs.html",{
-                "all_job":"adfadf"
+                "all_jobs":Circulars
         })
         else:
             return redirect(reverse('client_user:login'))
-    
+def postjob(request):
+    return render(request,"JobCircular/jobpost.html",{
+         "form":job()
+    })
