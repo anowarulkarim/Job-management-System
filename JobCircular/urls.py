66a4b25268ad63c2ea110c3838752c762705ba0a
anowarulkarim <anowarulkarim8@gmail.com>
2024-09-04 23:56:39 +0600

    updated recruiter home,login,signup

--- JobCircular/urls.py
+++ JobCircular/urls.py
@@ -3,5 +3,9 @@ from . import views
 app_name="jobs"
 urlpatterns=[
     path("",views.alljobs,name="allJobs"),
-    path("postjob/",views.postjob,name="postjob")
+    path("postjob/",views.postjob,name="postjob"),
+    path("recruterlogin/",views.recruterLogin,name="recruterlogin"),
+    path("recrutersignup/",views.recruterSignup,name="recrutersignup"),
+    path("recruterpanel/",views.recruterSignup,name="recrutersignup"),
+    path("recruterhome/",views.recruterhome,name="recruterhome")
 ]
--- JobCircular/views.py
+++ JobCircular/views.py
@@ -2,8 +2,19 @@ from django import forms
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
-from . models import Circular
+from . models import Circular,Recruter
+from django.contrib.auth.hashers import make_password,check_password
 # Create your views here.
+class recrutersignup(forms.Form):
+    name=forms.CharField(max_length=30)
+    company=forms.CharField(max_length=50)
+    designation=forms.CharField(max_length=20)
+    username=forms.CharField(max_length=20)
+    password=forms.CharField(max_length=20)
+    password2=forms.CharField(max_length=20)
+class recruterlogin(forms.Form):
+    username=forms.CharField(max_length=20)
+    pasword=forms.CharField(max_length=20)
 class job(forms.Form):
     titel=forms.CharField(max_length=50)
     salary=forms.IntegerField()
@@ -18,5 +29,56 @@ def alljobs(request):
             return redirect(reverse('client_user:login'))
 def postjob(request):
     return render(request,"JobCircular/jobpost.html",{
-         "form":job()
+        "form":job()
     })
+def recruterLogin(request):
+    if request.method=="POST":
+        login_form=recruterlogin(request.POST)
+        if login_form.is_valid():
+            username=login_form.cleaned_data["username"]
+            if Recruter.objects.filter(username=username).exists():
+                recruter=Recruter.objects.get(username=username)
+                password=login_form.cleaned_data["password"]
+                if check_password(password, recruter.password):
+                    request.session["recruter"]=recruter.username
+                    return redirect(reverse())
+
+    return render(request,"JObCircular/recruterlogin.html",{
+        "form":recruterlogin()
+    })
+def recruterSignup(request):
+    if request.method=="POST":
+        signup_form=recrutersignup(request.POST)
+        if signup_form.is_valid():
+            username=signup_form.cleaned_data["username"]
+            if Recruter.objects.filter(username=username).exists():
+                return render(request,"JobCircular/recrutersignup.html",{
+                    "form":recrutersignup(),
+                    "error":"username Taken"
+                })
+            elif signup_form.cleaned_data["password"] != signup_form.cleaned_data["password2"]:
+                return render(request,"JobCircular/recrutersignup.html",{
+                    "form":recrutersignup(),
+                    "error":"Password does not match"
+                })
+            else:
+                hashed_password = make_password(signup_form.cleaned_data["password"])
+                new_user = Recruter(
+                    name=signup_form.cleaned_data["name"],
+                    company=signup_form.cleaned_data["company"],
+                    designation=signup_form.cleaned_data["designation"],
+                    username=username,
+                    password=hashed_password,
+                )
+                new_user.save()
+                return redirect(reverse('jobs:recruterlogin'))
+        # return redirect(reverse('jobs:recruterlogin'))
+    else:
+        return render(request,"JobCircular/recrutersignup.html",{
+            "form":recrutersignup()
+        })
+def recruterhome(request):
+    if request.session.get("recruter")!=None:
+        return render(request,"JobCircular/recruterhome.html")
+    else:
+        return redirect(reverse('jobs:recruterlogin'))
